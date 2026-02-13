485db5b150f7222c0ee1ce144f4545d34979e63a
waloncycler <waloncycler@163.com>
2026-02-13 17:06:17 +0800

    Update deployment script port mapping to 80:3000

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,7 +33,7 @@ echo "Starting new container..."
 docker run -d \
     --name $CONTAINER_NAME \
     --restart always \
-    -p 80:3000 \
+    -p 3000:3000 \
     --env-file .env \
     $FULL_IMAGE
 
